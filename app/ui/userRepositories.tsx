7ad0cb1d51e369fd209341141e8d6a5ee8593d2f
Baltaguirre <baltaguirre@gmail.com>
2024-04-01 10:31:39 -0300

    fixed typo

--- app/ui/userRepositories.tsx
+++ app/ui/userRepositories.tsx
@@ -24,12 +24,13 @@ export async function UserView(searchParams: any) {
             {userRepos?.map((repo: any) => {
               return (
                 <a
-                  href={`https://github.com/${repo.full_name}`}
+                  href={`https://github.com/${repo?.full_name}`}
                   target="blank"
                   className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
+                key={repo?.id}
                 >
                   {" "}
-                  <li key={repo.id}>{repo?.name}</li>
+                  <li key={repo?.id}>{repo?.name}</li>
                 </a>
               );
             })}
